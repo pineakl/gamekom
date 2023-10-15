f858d2b95348ed5bedc9b776b648973eb3054c7b
Lukman Al <30369491+pineakl@users.noreply.github.com>
2023-10-15 18:16:54 +0700

    Revert "Extending sprites and add input"
    This reverts commit 7480b6d5197e70871dfdfabd4d006980a2de3e37.

--- main.lua
+++ main.lua
@@ -1,18 +1,17 @@
-PlayerObj = require("player");
-
-Player_ship = {}
-
 function love.load()
     love.window.setTitle("Void Trigger")
     love.window.setMode(320, 480);
 
-    Player_ship = Player.create(140, 300, 3);
+    Player_ship = {}
+    Player_ship.x = 140
+    Player_ship.y = 300
+    Player_ship.sprite = love.graphics.newImage("Assets/Sprites/ship-a1.png")
 end
 
 function love.update(dt)
-    Player.update(dt, Player_ship)
+    Player_ship.y = Player_ship.y - 1
 end
 
 function love.draw()
-    Player.draw(Player_ship)
+    love.graphics.draw(Player_ship.sprite, Player_ship.x, Player_ship.y)
 end
